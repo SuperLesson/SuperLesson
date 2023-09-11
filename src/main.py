6a74322a12d419f5acd9636456a8a11d7472f179
rsgrava <rsgrava@hotmail.com>
2023-09-11 18:41:47 -0300

    main: setup argument parser

--- src/main.py
+++ src/main.py
@@ -1,15 +1,22 @@
-from sys import argv
+from argparse import ArgumentParser
 
 from annotate import Annotate
 from transcribe import Transcribe
 from transitions import Transitions
 
 
-def main(lesson_id):
-    transcribe = Transcribe(lesson_id)
+def main():
+    parser = ArgumentParser(
+        prog="SuperLesson",
+        description="CLI to transcribe lectures",
+    )
+    parser.add_argument("lesson_id")
+    args = parser.parse_args()
+
+    transcribe = Transcribe(args.lesson_id)
     transcribe.single_file()
     input("Press Enter to continue...")
-    transitions = Transitions(lesson_id)
+    transitions = Transitions(args.lesson_id)
     transitions.insert_tmarks()
     input("Press Enter to continue...")
     transitions.verify_tbreaks_with_mpv()
@@ -20,9 +27,9 @@ def main(lesson_id):
     input("Press Enter to continue...")
     transcribe.check_differences()
     input("Press Enter to continue...")
-    annotate = Annotate(lesson_id)
+    annotate = Annotate(args.lesson_id)
     annotate.to_pdf()
 
 
 if __name__ == "__main__":
-    main(argv[1])
+    main()
