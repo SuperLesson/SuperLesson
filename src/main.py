c152dad606045c2990ef4895b7b7e81fe8629540
Isabella Basso <isabellabdoamaral@usp.br>
2023-09-15 15:31:55 -0300

    main: move argparsing to separate function

--- src/main.py
+++ src/main.py
@@ -1,22 +1,17 @@
-from argparse import ArgumentParser
+from argparse import ArgumentParser, Namespace
 
 from annotate import Annotate
 from transcribe import Transcribe
 from transitions import Transitions
 
 
-def main():
-    parser = ArgumentParser(
-        prog="SuperLesson",
-        description="CLI to transcribe lectures",
-    )
-    parser.add_argument("lesson_id")
-    args = parser.parse_args()
+def main(args: Namespace):
+    lesson_id = args.lesson_id
 
-    transcribe = Transcribe(args.lesson_id)
+    transcribe = Transcribe(lesson_id)
     transcribe.single_file()
     input("Press Enter to continue...")
-    transitions = Transitions(args.lesson_id)
+    transitions = Transitions(lesson_id)
     transitions.insert_tmarks()
     input("Press Enter to continue...")
     transitions.verify_tbreaks_with_mpv()
@@ -27,9 +22,19 @@ def main():
     input("Press Enter to continue...")
     transcribe.check_differences()
     input("Press Enter to continue...")
-    annotate = Annotate(args.lesson_id)
+    annotate = Annotate(lesson_id)
     annotate.to_pdf()
 
 
+def parse_args() -> Namespace:
+    parser = ArgumentParser(
+        prog="SuperLesson",
+        description="CLI to transcribe lectures",
+    )
+    parser.add_argument("lesson_id")
+    return parser.parse_args()
+
+
 if __name__ == "__main__":
-    main()
+    args = parse_args()
+    main(args)
