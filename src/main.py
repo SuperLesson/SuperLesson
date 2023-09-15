634fefefd8e9d5a8c9caf44d00fbf1688a1afb9a
Isabella Basso <isabellabdoamaral@usp.br>
2023-09-15 15:31:55 -0300

    utils: add support for multiple file types
    This also increases max-complexity on the flake8 file without taking
    into consideration the hack introduced in `_verify_files`.
    
    Co-authored-by: Isaque Alves <isaquealvesdl@gmail.com>

--- src/main.py
+++ src/main.py
@@ -3,17 +3,20 @@ from argparse import ArgumentParser, Namespace
 from annotate import Annotate
 from transcribe import Transcribe
 from transitions import Transitions
-from utils import LessonFiles
+from utils import FileType, LessonFiles
 
 
 def main(args: Namespace):
-    lesson_files = LessonFiles(args.lesson)
+    lesson_files = LessonFiles(args.lesson, args.transcribe_with, args.annotate_with)
     lesson_root = lesson_files.lesson_root
 
     transcribe = Transcribe(lesson_root)
-    transcription_source = lesson_files.transcription_source
+    transcription_source = lesson_files.transcription_source.path
     transcription_output = transcribe.single_file(transcription_source)
     input("Press Enter to continue...")
+    # TODO: Add option to use audio as source for transcription
+    if lesson_files.transcription_source.file_type == FileType.audio:
+        raise NotImplementedError("Transcribing from audio is not implemented yet")
     transitions = Transitions(lesson_root)
     tmarks_path = transitions.insert_tmarks(transcription_source, transcription_output)
     input("Press Enter to continue...")
@@ -27,6 +30,8 @@ def main(args: Namespace):
     improved_path = transcribe.improve_transcription(replacement_path)
     transcribe.check_differences(replacement_path, improved_path)
     input("Press Enter to continue...")
+    if lesson_files.lecture_notes.file_type == FileType.video:
+        raise NotImplementedError("Annotating from video is not implemented yet")
     annotate = Annotate(lesson_root)
     annotate.to_pdf(lesson_files.lecture_notes)
 
@@ -38,6 +43,14 @@ def parse_args() -> Namespace:
     )
     parser.add_argument("lesson",
                         help="Lesson name or path to lesson directory")
+    parser.add_argument("--transcribe-with",
+                        choices=[FileType.video.value, FileType.audio.value],
+                        default=None,
+                        help="Use audio or video file for transcription")
+    parser.add_argument("--annotate-with",
+                        choices=[FileType.notes.value, FileType.video.value],
+                        default=None,
+                        help="Use text/pdf or video as source for lecture notes")
     return parser.parse_args()
 
 
--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,49 @@
+import mimetypes
 import os
+from dataclasses import dataclass
+from enum import Enum
+from typing import Optional
+
+
+class FileType(Enum):
+    video = "video"
+    audio = "audio"
+    notes = "notes"
+
+
+@dataclass
+class LessonFile:
+    """Class to represent a lesson file."""
+
+    name: str
+    path: str
+    file_type: FileType
+
+    def __init__(self, name: str, path: str):
+        self.name = name
+        self.path = path
+        self.file_type = self._file_type(name)
+
+    @staticmethod
+    def _file_type(name: str) -> FileType:
+        """Return the file type of a given file name."""
+        mime_type, _ = mimetypes.guess_type(name)
+        if mime_type is None:
+            raise ValueError(f"File type not found for {name}")
+        mime_type = mime_type.split("/")[0]
+        match mime_type:
+            case "video":
+                file_type = FileType.video
+            case "audio":
+                file_type = FileType.audio
+            case "text":
+                file_type = FileType.notes
+            case _:
+                if name.endswith(".pdf"):
+                    file_type = FileType.notes
+                else:
+                    raise ValueError(f"File type not found for {name}")
+        return file_type
 
 
 class LessonFiles:
@@ -6,7 +51,9 @@ class LessonFiles:
 
     lesson_root: str
 
-    def __init__(self, lesson: str):
+    def __init__(self, lesson: str,
+                 transcribe_with: Optional[FileType] = None,
+                 annotate_with: Optional[FileType] = None):
         current_script_directory = os.path.dirname(os.path.abspath(__file__))
         project_root = os.path.abspath(os.path.join(current_script_directory, '..'))
 
@@ -18,23 +65,63 @@ class LessonFiles:
         else:
             raise ValueError(f"Lesson {lesson} not found")
 
-        files = self._get_files()
-        for file in files:
-            if file.endswith(f"{lesson}.mp4"):
-                self.transcription_source = file
-            elif file.endswith(f"{lesson}.pdf"):
-                self.lecture_notes = file
+        self._files: list[LessonFile] = []
+        self._transcribe_with = transcribe_with
+        self._annotate_with = annotate_with
+        self._transcription_source: Optional[LessonFile] = None
+        self._lecture_notes: Optional[LessonFile] = None
 
-    def _get_files(self) -> list[str]:
+    @property
+    def files(self) -> list[LessonFile]:
         """All usable files in lesson folder."""
+        if len(self._files) > 0:
+            return self._files
+
         print("Searching for files...")
-        lesson_files: list[str] = []
-        for root, _, files in os.walk(self.lesson_root):
-            for file in files:
+        for root, _, _files in os.walk(self.lesson_root):
+            for file in _files:
                 file_path = os.path.join(root, file)
                 try:
-                    lesson_files.append(file_path)
+                    self._files.append(LessonFile(file, file_path))
                 except ValueError:
                     pass
 
-        return lesson_files
+        # TODO: test for duplicate file types
+
+        return self._files
+
+    @property
+    def transcription_source(self) -> LessonFile:
+        """The file to be used for transcription."""
+        if self._transcription_source is None:
+            transcription_file = self._find_lesson_file([self._transcribe_with, FileType.video, FileType.audio])
+            if transcription_file is None:
+                raise ValueError(f"Transcription file not found on {self.lesson_root}")
+            self._transcription_source = transcription_file
+
+        return self._transcription_source
+
+    @property
+    def lecture_notes(self) -> LessonFile:
+        """The file to be used for annotation."""
+        if self._lecture_notes is None:
+            notes_file = self._find_lesson_file([self._annotate_with, FileType.notes, FileType.video])
+            if notes_file is None:
+                raise ValueError(f"Notes file not found on {self.lesson_root}")
+            self._lecture_notes = notes_file
+
+        return self._lecture_notes
+
+    def _find_lesson_file(self, accepted_types: list[Optional[FileType]]) -> Optional[LessonFile]:
+        for file_type in accepted_types:
+            if file_type is None:
+                continue
+            files = self._get_files(file_type)
+            if len(files) > 0:
+                return files[0]
+
+        return None
+
+    def _get_files(self, file_type: FileType) -> list[LessonFile]:
+        """Get all files of a given type."""
+        return [file for file in self.files if file.file_type == file_type]
