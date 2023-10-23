SuperLesson/SuperLesson

commit cb8da04c7dc4a3f89862f8db436f1d5072248535

transcribe: Add flag to define the model size
Closes: #22
Co-authored-by: Felipe Kaneshiro <felipek.souza@usp.br>
Co-authored-by: Isaque Alves <isaquealvesdl@usp.br>
Co-authored-by: Ricardo Saraiva Grava <rsgrava@hotmail.com>

superlesson/__init__.py
@@ -58,6 +58,14 @@ def parse_args() -> Namespace:
     parser.add_argument("--run-all",
                         action="store_true",
                         help="Run all steps")
+    parser.add_argument("--model-size",  # model only works with pt now
+                        choices=["tiny",  # "tiny.en",
+                                 "base",  # "base.en",
+                                 "small",  # "small.en",
+                                 "medium",  # "medium.en",
+                                 "large-v1", "large-v2", "large"],
+                        default="large-v2",
+                        help="Choose whisper model size")
     mut_group = parser.add_mutually_exclusive_group()
     mut_group.add_argument("--verbose", "-v",
                            action="store_true",

superlesson/steps/transcribe.py
@@ -15,14 +15,12 @@ class Transcribe:
         self.slides = slides
 
     @Step.step(Step.transcribe)
-    def single_file(self):
+    def single_file(self, model_size: str):
         from faster_whisper import WhisperModel
         from os import cpu_count
 
         bench_start = datetime.now()
 
-        # TODO: add a flag to select the model size
-        model_size = "large-v2"
         if self._has_nvidia_gpu():
             model = WhisperModel(model_size, device="cuda", compute_type="int8_float16")
         else:

commit 07139de0265d4e6c665d5bd99f5a57678d1de5be

superlesson: fix single_file call
Fixes: cb8da04c ("transcribe: Add flag to define the model size")

superlesson/__init__.py
@@ -16,7 +16,7 @@ def main():
 
     slides = Slides(lesson_files.lesson_root, args.run_all)
     transcribe = Transcribe(slides, lesson_files.transcription_source)
-    transcribe.single_file()
+    transcribe.single_file(args.model_size)
     input("Press Enter to continue...")
     # TODO: Add option to use audio as source for transcription
     if lesson_files.transcription_source.file_type == FileType.audio:
@@ -97,8 +97,8 @@ def single_step_setup(_class: Any) -> Tuple[Namespace, Any]:
 
 
 def transcribe_step():
-    _, transcribe = single_step_setup(Transcribe)
-    transcribe.single_file()
+    args, transcribe = single_step_setup(Transcribe)
+    transcribe.single_file(args.model_size)
 
 
 def tmarks_step():