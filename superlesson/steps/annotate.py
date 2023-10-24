43d4a3476a338e5ef2ba7a28eb55fdb6394d8999
Isaque Alves <isaquealvesdl@gmail.com>
2023-10-23 21:59:00 -0300

    annotation: highlight uncertain text
    Co-authored-by: Felipe Kaneshiro <felipek.souza@usp.br>

--- superlesson/steps/annotate.py
+++ superlesson/steps/annotate.py
@@ -36,6 +36,46 @@ class Annotate:
             merger.write(f)
         logging.info(f"Annotated PDF saved as {output}")
 
+    @staticmethod
+    def emphasize(text: str) -> str:
+        return "_" + text + "_"
+
+    @classmethod
+    def fade_slide(cls, current: str, next: str, threshold: int = 20) -> tuple[str, str]:
+        dots = ['.', '!', '?']
+
+        def text_before_dots(text: str, reverse: bool = False) -> tuple[int, int]:
+            text = reversed(text) if reverse else text
+            word_count = 0
+            last_index = 0
+            for index, char in enumerate(text):
+                if char == ' ':
+                    last_index = index
+                    word_count += 1
+                    continue
+                if word_count > threshold:
+                    break
+                if char in dots:
+                    if not reverse:
+                        last_index = index + 1
+                    break
+            return last_index, word_count
+
+        current_index, words_in_current = text_before_dots(current, True)
+        logging.debug(f"Fade in at word {words_in_current}")
+
+        next_index, words_in_next = text_before_dots(next, False)
+        logging.debug(f"Fade out at word {words_in_next}")
+
+        current_emphasized = cls.emphasize(current[-current_index:] + " \u21E2")
+        logging.debug(f"Fade in text: {current_emphasized}")
+        current = current[:-current_index] + current_emphasized
+        next_emphasized = cls.emphasize("\u21E2 " + next[:next_index])
+        logging.debug(f"Fade out text: {next_emphasized}")
+        next = next_emphasized + next[next_index:]
+
+        return current, next
+
     def _transcription_to_pdf(self, w) -> str:
         import typst
         preamble = f"""
@@ -66,12 +106,23 @@ class Annotate:
     linebreaks: "optimized",
 )
 
+#show emph: it => (
+  text(rgb("#3f3f3f"), it.body)
+)
+
 """
+        formatted_texts: list[str] = []
+        current_text = self.slides[0].transcription
+        for next in range(1, len(self.slides)):
+            current_text, next_text = self.fade_slide(current_text, self.slides[next].transcription)
+            formatted_texts.append(current_text)
+            current_text = next_text
+        formatted_texts.append(current_text)
         with tempfile.NamedTemporaryFile(delete=False) as f:
             f.write(preamble.encode("utf-8"))
             f.write(
                 "\n#pagebreak()\n".join(
-                    [slide.transcription for slide in self.slides]
+                    [text for text in formatted_texts]
                 ).encode("utf-8")
             )
             temp_file_name = f.name
