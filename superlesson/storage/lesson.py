SuperLesson/SuperLesson

commit 7b8bc290411b01bfba4f52cc95f49095775afce7

storage: ignore generated files
Closes: #72
Co-authored-by: Felipe Kaneshiro <felipek.souza@usp.br>

superlesson/storage/lesson.py
@@ -6,6 +6,8 @@ from enum import Enum
 from pathlib import Path
 from typing import List, Optional
 
+from .store import Store
+
 
 class FileType(Enum):
     video = "video"
@@ -88,10 +90,14 @@ class LessonFiles:
             return self._files
 
         logging.info("Searching for files...")
+        ignored = list(Store.txt_files())
+        ignored.append("transcription.pdf")
         for file in self.lesson_root.iterdir():
+            if file.name in ignored:
+                continue
             try:
-                logging.debug(f"Found file: {file}")
                 self._files.append(LessonFile(file.name, self.lesson_root))
+                logging.debug(f"Found file: {file}")
             except ValueError:
                 pass
 

superlesson/storage/store.py
@@ -40,6 +40,10 @@ class Store:
         self._storage_root = lesson_root / ".data"
         self._run_all = run_all
 
+    @classmethod
+    def txt_files(cls) -> List[str]:
+        return [f"{file.name}.txt" for file in cls._storage_map.values()]
+
     def in_storage(self, step: Step) -> bool:
         return self._storage_map.get(step) is not None
 

commit 8c8b4d954695e8a88f461dfd122cf475a475c00f

lesson: fix ignored pdf file
As we changed the name for the final annotated file, we have to update
it as well in the list of ignored files.

Fixes: 7b8bc290 ("storage: ignore generated files")

superlesson/storage/lesson.py
@@ -91,7 +91,7 @@ class LessonFiles:
 
         logging.info("Searching for files...")
         ignored = list(Store.txt_files())
-        ignored.append("transcription.pdf")
+        ignored.append("annotations.pdf")
         for file in self.lesson_root.iterdir():
             if file.name in ignored:
                 continue