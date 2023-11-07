5b00c9ba093c70f03c0214e544e282c9e8e246bf
Isabella Basso <isabellabdoamaral@usp.br>
2023-11-06 22:41:43 -0300

    fix slide numbering
    As tframes actually refer to the last moment before a change, fix png
    paths storage in merge and prompts in enumerate.
    
    Closes: #102

--- superlesson/steps/transitions.py
+++ superlesson/steps/transitions.py
@@ -53,8 +53,10 @@ class Transitions:
         # use this to merge the last slides
         self.slides.merge()
 
+        assert len(self.slides) == len(png_paths) + 1
+
         for i, path in enumerate(png_paths):
-            self.slides[i + 1].png_path = path
+            self.slides[i].png_path = path
 
     def _get_png_paths(self) -> list[Path]:
         tt_directory = self._transcription_source.path / "tframes"
