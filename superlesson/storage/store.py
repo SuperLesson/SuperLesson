SuperLesson/SuperLesson

commit fad9a14c35d0e763c8039ed66de1cba73b6e9473

step:  create a manual file for review

superlesson/storage/store.py
@@ -29,11 +29,34 @@ class Store:
         return self._lesson_root / f"{filename}.txt"
 
     @staticmethod
-    def _parse_txt(txt_path: Path) -> list[str]:
-        return [
-            format_transcription(text)
-            for text in re.split(r"====== SLIDE .* ======", txt_path.read_text())[1:]
-        ]
+    def _parse_txt(txt_path: Path) -> dict[str, Any]:
+            split_texts = re.split(r"====== SLIDE (\S*) \((.*)\) ======", txt_path.read_text())[1:]
+
+            transcriptions = split_texts[2::3]
+
+            timeframes = [
+             {"start": start, "end": end}
+	                for start, end in map(lambda t: t.split(" - "), split_texts[1::3])
+                    ]
+
+            slide_numbers = split_texts[::3]
+            def parse_to_int(value):
+                try:
+                    return int(value)
+                except (ValueError, TypeError):
+                    return None
+
+            transcription_dicts = []
+
+            for i in range(len(transcriptions)):
+                transcription_dict = {
+                    'transcription': format_transcription(transcriptions[i]),
+                    'timeframe': timeframes[i],
+                    'number': parse_to_int(slide_numbers[i])
+                }
+                transcription_dicts.append(transcription_dict)
+
+            return transcription_dicts
 
     @staticmethod
     def _parse_json(json_path: Path) -> list[dict[str, Any]]:
@@ -47,30 +70,25 @@ class Store:
 
         return data
 
-    def load(self, filename: str, load_txt: bool) -> Optional[list[Any]]:
-        json_path = self._get_storage_path(filename, Format.json)
-        if not json_path.exists():
-            return None
+    @classmethod
+    def load(cls, filename: str, load_txt: bool) -> Optional[list[Any]]:
+        if load_txt:
+            txt_path = cls._get_storage_path(filename, Format.txt)
+            if txt_path.exists():
+                logger.info(f"Loading {txt_path}")
+                return cls._parse_txt(txt_path)
 
-        logger.debug(f"Loading {json_path}")
-        data = self._parse_json(json_path)
+            else:
+                logger.info(
+                    f"Couldn't load from file {txt_path}, make sure it's properly formatted"
+                )
 
-        txt_path = self._get_storage_path(filename, Format.txt)
-        if not load_txt or not txt_path.exists():
-            return data
+        json_path = cls._get_storage_path(filename, Format.json)
+        if json_path.exists():
+            logger.info(f"Loading {json_path}")
+            return cls._parse_json(json_path)
 
-        logger.info(f"Loading {txt_path}")
-        transcriptions = self._parse_txt(txt_path)
-
-        if len(transcriptions) == len(data):
-            for i in range(len(data)):
-                data[i]["transcription"] = transcriptions[i]
-        else:
-            logger.warning(
-                f"Couldn't load from file {txt_path}, make sure it's properly formatted"
-            )
-
-        return data
+        return None
 
     @staticmethod
     def temp_save(txt_data: Any) -> Path:

superlesson/steps/step.py
@@ -28,6 +28,7 @@ class Step(Enum):
     enumerate = StepMetadata(name="enumerate slides", filename="enumerated")
     replace = StepMetadata(name="replace words", filename="replaced")
     improve = StepMetadata(name="improve punctuation", filename="improved")
+    manual = StepMetadata(name="manual revision", filename="manual")
     # TODO: annotated pdf should be managed by storage layer
     annotate = StepMetadata(name="annotate")
 

superlesson/storage/slide.py
@@ -153,10 +153,9 @@ class Slides(UserList):
         meta = step.value
         assert meta.filename is not None
         logger.debug(f"Loading data from step {step.value.name}")
-        verbose = step is not Step.transcribe
-        data = self._store.load(meta.filename, verbose)
+        data = self._store.load(meta.filename, load_txt=step > Step.merge)
         if data is not None:
-            self._load_slides(data, verbose)
+            self._load_slides(data, verbose=step is Step.transcribe)
             self._step_in_memory = step
             return True
         return False

commit 515d7a28382c70c77e157a622cbe9dfe68ed47ef

store: fix timestamp parsing when importing from txt
Fixes: fad9a14c ("step:  create a manual file for review")

Co-authored-by: Isaque Alves <isaquealvesdl@gmail.com>

superlesson/storage/store.py
@@ -3,6 +3,7 @@ import logging
 import re
 from enum import Enum, unique
 from pathlib import Path
+from datetime import time
 from typing import Any, Optional
 
 from .utils import format_transcription
@@ -36,8 +37,17 @@ class Store:
 
         transcriptions = split_texts[2::3]
 
+        def timestamp_to_seconds(timestamp: str) -> float:
+            if len(timestamp.split(":")[0]) == 1:
+                timestamp = "0" + timestamp
+            t = time.fromisoformat(timestamp)
+            return (t.hour * 60 + t.minute) * 60 + t.second + t.microsecond / 1e6
+
         timeframes = [
-            {"start": start, "end": end}
+            {
+                "start": timestamp_to_seconds(start),
+                "end": timestamp_to_seconds(end),
+            }
             for start, end in map(lambda t: t.split(" - "), split_texts[1::3])
         ]
 