50f32a21961bb6839cf70430c90f64f0630ea553
Isabella Basso <isabellabdoamaral@usp.br>
2023-12-04 18:57:16 -0300

    cli: use click instead of argparse
    Closes: #134
    Co-authored-by: Isaque Alves <isaquealvesdl@gmail.com>

--- README.md
+++ README.md
@@ -3,7 +3,6 @@
 [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
 [![Python Versions](https://img.shields.io/badge/Python-3.10%2C%203.11%2C%203.12-blue)](https://www.python.org/downloads/)
 [![Contributor Covenant](https://img.shields.io/badge/Contributor%20Covenant-2.1-4baaaa.svg)](code_of_conduct.md)
-
 ## Setup
 
 SuperLesson (SL) uses [WhisperX](hhttps://github.com/m-bain/whisperX) through [Replicate](https://replicate.com) to transcribe audio.
--- src/superlesson/cli.py
+++ src/superlesson/cli.py
@@ -1,13 +1,13 @@
 import logging
-from argparse import ArgumentParser, Namespace
-from pathlib import Path
-from typing import Any
+import subprocess
+from dataclasses import dataclass
 
-from .steps import Annotate, Transcribe, Transitions
+import click
+
 from .steps.step import Step
 from .storage import LessonFiles, Slides
 from .storage.lesson import FileType
-from .storage.utils import diff_words, find_lesson_root
+from .storage.utils import find_lesson_root
 
 logging.basicConfig(
     format="%(asctime)s.%(msecs)03d - %(name)s:%(levelname)s: %(message)s",
@@ -17,112 +17,133 @@ logging.basicConfig(
 logger = logging.getLogger("superlesson")
 
 
-def main():
-    args = parse_args()
-    set_log_level(args)
+@dataclass
+class Context:
+    lesson_files: LessonFiles
+    slides: Slides
 
-    if args.diff is not None:
-        step1 = Step[args.diff[0]]
-        step2 = Step[args.diff[1]]
-        if step1 == step2:
-            raise Exception("Cannot compare the same step")
-        if step1 > step2:
-            step1, step2 = step2, step1
-        check_differences(args.lesson, step1, step2)
+
+@click.group(invoke_without_command=True)
+@click.argument("lesson")
+@click.option(
+    "--transcribe-with",
+    "-t",
+    type=click.Path(),
+    help="Path for transcribe configuration.",
+)
+@click.option(
+    "--annotate-with", "-a", type=click.Path(), help="Path for annotate configuration."
+)
+@click.option("--verbose", "-v", is_flag=True, help="Enables verbose mode.")
+@click.option("--debug", is_flag=True, help="Enables verbose mode.")
+@click.pass_context
+def cli(ctx, lesson, transcribe_with, annotate_with, verbose, debug):
+    """Your CLI application for processing lessons."""
+    if ctx.invoked_subcommand == "diff":
         return
 
-    lesson_files = LessonFiles(args.lesson, args.transcribe_with, args.annotate_with)
+    lesson_files = LessonFiles(lesson, transcribe_with, annotate_with)
+    ctx.obj = Context(
+        lesson_files,
+        Slides(lesson_files.lesson_root, verbose),
+    )
 
-    slides = Slides(lesson_files.lesson_root, args.debug)
-    transcribe = Transcribe(slides, lesson_files.transcription_source)
-    transcribe.single_file()
+    if debug:
+        logger.setLevel(logging.DEBUG)
+    elif verbose:
+        logger.setLevel(logging.INFO)
+
+    if ctx.invoked_subcommand is not None:
+        return
+
+    ctx.invoke(transcribe)
     input("Press Enter to merge segments.")
-    # TODO: Add option to use audio as source for transcription
-    if lesson_files.transcription_source.file_type == FileType.audio:
-        raise NotImplementedError("Transcribing from audio is not implemented yet")
-    transitions = Transitions(slides, lesson_files.transcription_source)
-    transitions.merge_segments()
+    ctx.invoke(merge)
     input("Press Enter to replace words.")
-    transcribe.replace_words()
-    input("Press Enter to improve punctuation.")
-    transcribe.improve_punctuation()
+    ctx.invoke(replace)
+    input("Press Enter to improve text.")
+    ctx.invoke(improve)
     input("Press Enter to enumerate slides.")
-    annotate = Annotate(slides, lesson_files.presentation)
-    annotate.enumerate_slides_from_tframes()
+    ctx.invoke(enumerate)
     input("Press Enter to annotate.")
     if lesson_files.presentation.file_type == FileType.video:
-        raise NotImplementedError("Annotating from video is not implemented yet")
+        msg = "Annotating from video is not implemented yet"
+        raise NotImplementedError(msg)
+    ctx.invoke(annotate)
+
+
+@cli.command()
+@click.pass_context
+def transcribe(ctx):
+    """Transcribe a LESSON."""
+    from .steps import Transcribe
+
+    Transcribe(ctx.obj.slides, ctx.obj.lesson_files.transcription_source).single_file()
+
+
+@cli.command()
+@click.pass_context
+def merge(ctx):
+    """Merge words."""
+    from .steps import Transitions
+
+    Transitions(
+        ctx.obj.slides, ctx.obj.lesson_files.transcription_source
+    ).merge_segments()
+
+
+@cli.command()
+@click.pass_context
+def enumerate(ctx):
+    """Enumerate slides."""
+    from .steps import Annotate
+
+    Annotate(
+        ctx.obj.slides, ctx.obj.lesson_files.presentation
+    ).enumerate_slides_from_tframes()
+
+
+@cli.command()
+@click.pass_context
+def replace(ctx):
+    """Replace bogus words."""
+    from .steps import Transcribe
+
+    Transcribe(
+        ctx.obj.slides, ctx.obj.lesson_files.transcription_source
+    ).replace_words()
+
+
+@cli.command()
+@click.pass_context
+def improve(ctx):
+    """Improve text."""
+    from .steps import Transcribe
+
+    Transcribe(
+        ctx.obj.slides, ctx.obj.lesson_files.transcription_source
+    ).improve_punctuation()
+
+
+@cli.command()
+@click.pass_context
+def annotate(ctx):
+    """Annotate to PDF."""
+    from .steps import Annotate
+
+    annotate = Annotate(ctx.obj.slides, ctx.obj.lesson_files.presentation)
     annotate.to_pdf()
 
 
-def parse_args() -> Namespace:
-    parser = ArgumentParser(
-        prog="SuperLesson",
-        description="CLI to transcribe lectures",
-    )
-    parser.add_argument("lesson", help="Lesson name or path to lesson directory")
-    parser.add_argument(
-        "--diff",
-        default=None,
-        nargs=2,
-        choices=[step.name for step in Step if step.value.in_storage()],
-        help="Diff between two steps",
-    )
-    parser.add_argument(
-        "--transcribe-with",
-        type=Path,
-        default=None,
-        help="Path to transcription source (has to be a video file)",
-    )
-    parser.add_argument(
-        "--annotate-with",
-        type=Path,
-        default=None,
-        help="Path to annotation source (has to be a PDF file)",
-    )
-    mut_group = parser.add_mutually_exclusive_group()
-    mut_group.add_argument(
-        "--verbose", "-v", action="store_true", help="Increase output verbosity"
-    )
-    mut_group.add_argument(
-        "--debug",
-        "-d",
-        action="store_true",
-        help="Print debug information and save step outputs as txt",
-    )
-    return parser.parse_args()
-
-
-def set_log_level(args: Namespace):
-    if args.debug:
-        logger.setLevel(logging.DEBUG)
-    elif args.verbose:
-        logger.setLevel(logging.INFO)
-
-
-def single_step_setup(_class: Any) -> tuple[Namespace, Any]:
-    args = parse_args()
-    set_log_level(args)
-
-    lesson_files = LessonFiles(args.lesson, args.transcribe_with, args.annotate_with)
-
-    slides = Slides(lesson_files.lesson_root, args.debug)
-    if _class is Annotate:
-        instance = _class(slides, lesson_files.presentation)
-    else:
-        instance = _class(slides, lesson_files.transcription_source)
-    return args, instance
-
-
-def transcribe_step():
-    _, transcribe = single_step_setup(Transcribe)
-    transcribe.single_file()
-
-
-def check_differences(lesson: str, prev: Step, next: Step):
+@cli.command()
+@click.argument("lesson")
+@click.argument("previous", type=click.Choice([step.name for step in Step]))
+@click.argument("next", type=click.Choice([step.name for step in Step]))
+def diff(lesson, previous, next):
+    """Show differences in a LESSON from ."""
     lesson_root = find_lesson_root(lesson)
     prev_slides = Slides(lesson_root)
-    prev_slides.load_step(prev)
+    prev_slides.load_step(previous)
     prev_file = prev_slides.save_temp_txt()
 
     next_slides = Slides(lesson_root)
@@ -130,29 +151,20 @@ def check_differences(lesson: str, prev: Step, next: Step):
     next_file = next_slides.save_temp_txt()
 
     logger.debug("Running wdiff")
-    diff_words(prev_file, next_file)
+
+    subprocess.run(
+        " ".join(
+            [
+                "wdiff",
+                "-n -w $'\033[30;41m' -x $'\033[0m' -y $'\033[30;42m' -z $'\033[0m'",
+                str(prev_file),
+                str(next_file),
+            ]
+        ),
+        shell=True,
+    )
 
 
-def merge_step():
-    _, transitions = single_step_setup(Transitions)
-    transitions.merge_segments()
-
-
-def replace_step():
-    _, transcribe = single_step_setup(Transcribe)
-    transcribe.replace_words()
-
-
-def improve_step():
-    _, transcribe = single_step_setup(Transcribe)
-    transcribe.improve_punctuation()
-
-
-def enumerate_step():
-    _, annotate = single_step_setup(Annotate)
-    annotate.enumerate_slides_from_tframes()
-
-
-def annotate_step():
-    _, annotate = single_step_setup(Annotate)
-    annotate.to_pdf()
+if __name__ == "__main__":
+    print("meow")
+    cli()
